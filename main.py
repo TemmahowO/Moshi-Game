ba19ed9bdcc2d46a02fba3091419e73dcaf053c4
Temmie Peach (Alias) <twemmie.owo123@gmail.com>
2022-12-04 13:22:25 +0000

    n/a

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# re-write whatever I wrote in the prototype branch and make it readeble and sustainable.
+import pygame
+from random import randint
+
+pygame.init()
+
+window_width = 500
+window_height = 500
+
+object_y = 0
+object_x = 0
+player_x = 0
+player_y = 0
+
+window = pygame.display.set_mode()
