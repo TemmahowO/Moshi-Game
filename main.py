5efc3a001d05f13645397b2288d4071cd701db6f
Temmie Peach (Alias) <twemmie.owo123@gmail.com>
2022-12-04 13:33:10 +0000

    WIP commit

--- main.py
+++ main.py
@@ -11,4 +11,22 @@ object_x = 0
 player_x = 0
 player_y = 0
 
+Game_on = True
+
 window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Moshi Game")
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        pygame.draw.rect()
+
+while Game_on == True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Game_on = False
+
+
+pygame.quit()
+quit()
