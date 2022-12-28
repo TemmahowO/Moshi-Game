0c59d3b4cbd44aea306de12d6134f8c1ea83032c
Temmie <twemmie.owo123@gmail.com>
2022-12-28 15:25:28 +0000

    Re-design change (Stable commit - 28/12/22)
    Completely re-designed the game. Exploration and the camera function I
    have been working on went completely out of the window. I re-wrote
    the Object class, not changing much in term of function but still.
    
    I also added a "if main = name then" thing to main.py and made
    the movement controller also responsible for collision detection.
    
    Deleted a lot of useless variables that were no longer needed and made
    the entire game loop body a lot shorter.
    
    - Tem

--- Classes.py
+++ Classes.py
@@ -1,56 +1,45 @@
 import pygame
-from pygame.locals import *
+from pygame.locals import Rect
 from random import randint
 
 class Player:
-    def __init__(self):
-        self.player_x = randint(-500, 500)
-        self.player_y = randint(-500, 500)
-        self.player_speed = 0
-        self.health = 100
+    def __init__(self, size):
+        self.x_pos = randint(0, 500)
+        self.y_pos = randint(0, 500)
         self.hunger = 50
-      
+        self.health = 100
+        self.size = size
+
     def health_system(self):
         self.hunger -= .1
         if self.hunger <= 0:
             self.hunger = 0
             self.health -= 1
 
+    def draw(self, window, colour, size):
+        self.draw_x = self.x_pos
+        self.draw_y = self.y_pos
+        self.rect = Rect((self.x_pos, self.y_pos, size, size))
+        
+        pygame.draw.rect(window, colour, self.rect)
+        return self.rect
+
+    def draw_hitbox(self, window, colour, player_size):
+        self.hitbox_rect = Rect(self.x_pos - player_size /2, self.y_pos - player_size /2, 20, 20) # Draws the hitbox at the center of the player.
+        pygame.draw.rect(window, colour, self.hitbox_rect)
+        return self.hitbox_rect
 
 class Object:
-    def __init__(self):
-        self.object_x = randint(0, 500)
-        self.object_y = randint(100, 500)
-        self.y_speed = 0
-        self.x_speed = 0
-        self.colour = (255, 255, 255)
-        self.default_size = 10
-        self.object_rect = Rect((self.object_x, self.object_y, self.default_size, self.default_size))
+    def __init__(self, durability, size):
+        self.x_pos = randint(0, 500)
+        self.y_pos = randint(0, 500)
+        self.durability = durability
+        self.size = size
 
-    def draw(self, window, size):
-        self.draw_x = self.object_x
-        self.draw_y = self.object_y
-        self.object_rect = Rect((self.object_x, self.object_y, size, size))
+    def draw(self, window, colour, size):
+        self.draw_x = self.x_pos
+        self.draw_y = self.y_pos
+        self.rect = Rect((self.x_pos, self.y_pos, size, size))
         
-        pygame.draw.rect(window, self.colour, self.object_rect)
-
-    # For the camera
-    def surface_crossover(self):
-        if self.object_x <= -10:
-            self.object_x = 810
-            self.object_y = randint(0, 700)
-
-        elif self.object_x >= 810:
-            self.object_x = -10
-            self.object_y = randint(0, 700)
-            
-        if self.object_y <= -10:
-            self.object_y = 710
-            self.object_x = randint(0, 800)
-        elif self.object_y >= 710:
-            self.object_y = -10
-            self.object_x = randint(0, 800)
-
-class Tree(Object):
-    pass
-
+        pygame.draw.rect(window, colour, self.rect)
+        return self.rect
--- main.py
+++ main.py
@@ -1,82 +1,45 @@
 import pygame
 from pygame.locals import *
+import sys
 from Classes import * 
 import Functions
 
-white = (255, 255, 255)
-red = (255, 0, 0)
-gray = (169,169,169)
-black = (0, 0, 0)
+def main():
+    pygame.init()
 
-window_width = 800
-window_height = 700
-fps = 30
-clock = pygame.time.Clock()
-rand_x = 100
-rand_y = 400
+    white = (255, 255, 255)
+    red = (255, 0, 0)
+    gray = (169,169,169)
+    black = (0, 0, 0)
 
-object_y = 240
-object_x = 240
-player_x_pos = 250
-player_y_pos = 250
+    window_width = 800
+    window_height = 700
+    fps = 60
+    clock = pygame.time.Clock()
+    Game_on = True
 
-player_width = 10
-player_hight = 10
-object_width = 10
-object_hight = 10
+    player = Player(10)
+    objects = Object(5, 10)
 
-Game_on = True
+    window = pygame.display.set_mode((window_width, window_height), pygame.RESIZABLE)
+    pygame.display.set_caption("Moshi Game")
 
-# Defined here to avoid confusion. - FIX THIS LATER.
-objects = Object()
-player = Player()
+    while Game_on == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                Game_on = False
 
-pygame.init()
+        print(f"Player X: {player.x_pos}, Player Y: {player.y_pos}")
+        window.fill(gray)
+        player.draw_hitbox(window, white, 10) # hitbox
+        player.draw(window, black, 10) # player
+        objects.draw(window, red, 20)
+        Functions.movement_controller_and_collision_check(player, player.rect, objects.rect) # Called here so it can use the retunred rect values
+        clock.tick(fps)
+        pygame.display.update()
 
-fonts = pygame.font.get_fonts()
+    pygame.quit()
+    sys.exit()
 
-window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Moshi Game")
-
-
-while Game_on == True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            Game_on = False
-    
-    player_rect = Rect((player_x_pos, player_y_pos, player_width, player_hight))
-    collide_true = pygame.Rect.colliderect(player_rect, objects.object_rect)
-
-    # Calling all functions from within the Functions.py file.
-    Functions.movement_controller()
-    Functions.write_to_json()
-    Functions.render_from_json()
-    Functions.objects.surface_crossover()
-    
-
-    #Changing Objects and Players x and y coords.
-    objects.object_x += objects.x_speed
-    objects.object_y += objects.y_speed
-    player.player_x += objects.x_speed
-    player.player_y += objects.y_speed
-    print(f"object_x: {objects.object_x}\n object_y: {objects.object_y}")
-
-    # Collision checking. Everything else was too complicated.
-    if collide_true == True:
-        rand_x2 = randint(rand_x, rand_y)
-        rand_y2 = randint(rand_x, rand_y)
-        objects.object_y = rand_y2
-        objects.object_x = rand_x2
-
-
-    window.fill(gray)
-    objects.draw(window, 20)
-    pygame.draw.rect(window, red, player_rect)
-    Functions.message_to_screen(window, f"player_x: {player.player_x}", red, 0, 0) # Exists for debugging reasons. 
-    Functions.message_to_screen(window, f"player_y: {player.player_y}", red, 0, 20) # Exists for debugging reasons. 
-    clock.tick(fps)
-    pygame.display.update()
-
-
-pygame.quit()
-quit()
+if __name__ == "__main__":
+    main()
